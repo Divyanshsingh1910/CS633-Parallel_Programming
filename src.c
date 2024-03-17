91ad2d50fef006b0ee179bc103fe296527cb3819
Rajeev Kumar <rajeevks21@iitk.ac.in>
2024-03-17 16:15:02 +0530

    Added stencil computation

--- src.c
+++ src.c
@@ -1,44 +1,62 @@
 #include <stdio.h>
 #include "mpi.h"
+#include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
 
-int cols,rows,Px,Py,myrank;
-double** data;
-
-
-
-bool    has_left_neighbour = true,
-        has_right_neighbour = true,
-        has_top_neighbour = true,
-        has_bottom_neighbour = true;
+int 	cols, rows, P, Px, Py, myrank;
+double	**data;
+double	*from_left, *from_right, *from_top, *from_bottom;
+bool    has_left_neighbour = true, has_right_neighbour = true, has_top_neighbour = true, has_bottom_neighbour = true;
 
 void fill_has_neighbours()
 {
-    if(myrank%Px == 0) 					// 0, 3, 6, 9
+    if(myrank%Px == 0) // 0, 3, 6, 9
         has_left_neighbour = false;
-    if(myrank%Px == Px - 1) 			// 2, 5, 8, 11
+    if(myrank%Px == Px - 1) // 2, 5, 8, 11
         has_right_neighbour = false;
-        
-    if(myrank/Px == 0) 					// 0, 1, 2
+    if(myrank/Px == 0) // 0, 1, 2
         has_top_neighbour = false;
-    if(myrank/Px == Py - 1) 			// 9, 10, 11
+    if(myrank/Px == Py - 1) // 9, 10, 11
         has_bottom_neighbour = false;
 }
 
-int get_nneighbours(int i, int j, data)
+double get_val(int i, int j)
 {
-    int nneighbours = 4;
-    if(i == 0 && !has_top_neighbour)
-        nneighbours--;
-    if(i == rows - 1 && !has_bottom_neighbour)
-        nneighbours--;
-    if(j == 0 && !has_left_neighbour)
-        nneighbours--;
-    if(j == cols - 1 && !has_right_neighbour)
-        nneighbours--;
-    return nneighbours;
+	return (i >= 0 && i < rows && j >= 0 && j < cols) ? data[i][j] : 0;
 }
+
+void compute(int i, int j)
+{
+    double sum = 0;
+	int nneighbours = 4;
+    if(i == 0){
+		if(has_top_neighbour)
+			sum += from_top[j];
+		else
+			nneighbours--;
+    }
+    if(i == rows - 1){
+		if(has_bottom_neighbour)
+			sum += from_bottom[j];
+        else
+			nneighbours--;
+	}
+    if(j == 0){
+		if(has_left_neighbour)
+			sum += from_left[i];
+		else
+        	nneighbours--;
+	}
+    if(j == cols - 1){
+		if(has_right_neighbour)
+			sum += from_right[i];
+		else
+        	nneighbours--;
+	}
+	data[i][j] = (data[i][j] + get_val(i, j - 1) + get_val(i, j + 1) + get_val(i - 1, j) + get_val(i + 1, j))/(nneighbours + 1);
+}
+
 int main(int argc, char *argv[]) 
 {
 	// initialize MPI
@@ -47,7 +65,6 @@ int main(int argc, char *argv[])
 	MPI_Status status;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	fill_has_neighbours();
 
@@ -62,7 +79,7 @@ int main(int argc, char *argv[])
 	rows = sqrt(N), cols = rows;
 	
 
-	double** data = (double **)malloc(rows*sizeof(double*));
+	data = (double **)malloc(rows*sizeof(double*));
 	
 	for(int i=0; i<rows; i++){
 		data[i] = (double *)malloc(cols*sizeof(double));
@@ -75,14 +92,12 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	//printf("ex: %f,%f\n",data[56][89],data[464][46]);
 	/* Communicated data */
 	from_left = has_left_neighbour? (double*)malloc(rows*sizeof(double)):NULL;
 	from_right = has_right_neighbour? (double*)malloc(rows*sizeof(double)):NULL;
 
 	from_top = has_top_neighbour? (double*)malloc(cols*sizeof(double)):NULL;
 	from_bottom = has_bottom_neighbour? (double*)malloc(cols*sizeof(double)):NULL;
-
 
 along_x_communication:
 		
@@ -91,7 +106,7 @@ along_x_communication:
 		switch((myrank%Px) % 2){
 		
 		case(0):
-			if(myrank%Px != Px-1){
+			if(has_right_neighbour){
 				/* Sending to right */
 				MPI_Send(&data[i][cols-1], 1 , MPI_DOUBLE, myrank+1 , i,MPI_COMM_WORLD );
 
@@ -101,7 +116,7 @@ along_x_communication:
 			break;
 
 		case(1):
-			if(myrank%Px != 0){
+			if(has_left_neighbour){
 				/* Receiving from left */
 				MPI_Recv(&from_left[i], 1 , MPI_DOUBLE, myrank-1 , i , MPI_COMM_WORLD, &status);
 
@@ -117,7 +132,7 @@ along_x_communication:
 		switch((myrank%Px) % 2){
 		
 		case(0):
-			if(myrank%Px != 0){
+			if(has_left_neighbour){
 				/* Sending to left */
 				MPI_Send(&data[i][0], 1 , MPI_DOUBLE, myrank-1 , i,MPI_COMM_WORLD );
 
@@ -127,7 +142,7 @@ along_x_communication:
 			break;
 
 		case(1):
-			if(myrank%Px != Px-1){
+			if(has_right_neighbour){
 				/* Receiving from right */
 				MPI_Recv(&from_right[i], 1 , MPI_DOUBLE, myrank+1 , i , MPI_COMM_WORLD, &status);
 
@@ -153,7 +168,7 @@ along_y_communication:
 		switch( (myrank/Px) % 2 ){
 		
 		case(0):
-			if(myrank/Px != Py-1){
+			if(has_bottom_neighbour){
 				/* Sending to below */
 				MPI_Send(&data[rows-1][j], 1 , MPI_DOUBLE, myrank+Px , j ,MPI_COMM_WORLD );
 
@@ -163,7 +178,7 @@ along_y_communication:
 			break;
 
 		case(1):
-			if(myrank/Px != 0){
+			if(has_top_neighbour){
 				/* Receiving from top */
 				MPI_Recv(&from_top[j], 1 , MPI_DOUBLE, myrank-Px , j , MPI_COMM_WORLD, &status);
 
@@ -179,7 +194,7 @@ along_y_communication:
 		switch( (myrank/Px) % 2 ){
 		
 		case(0):
-			if(myrank/Px != 0){
+			if(has_top_neighbour){
 				/* Sending to top */
 				MPI_Send(&data[0][j], 1 , MPI_DOUBLE, myrank-Px , j ,MPI_COMM_WORLD );
 
@@ -189,7 +204,7 @@ along_y_communication:
 			break;
 
 		case(1):
-			if(myrank/Px != Py-1){
+			if(has_bottom_neighbour){
 				/* Receiving from below */
 				MPI_Recv(&from_bottom[j], 1 , MPI_DOUBLE, myrank+Px , j , MPI_COMM_WORLD, &status);
 
@@ -202,26 +217,13 @@ along_y_communication:
 
 	
 stencil_computation:
-
-	
-	int row_parity = rank/Px; /* {0,1,2,...,Py-1} */
-	int col_parity = rank%Px; /* {0,1,...,Px-1} */
-
 	for(int i=0; i<rows; i++){
 		for(int j=0; j<cols; j++){
-			if(i>0 && i<rows-1 && j>0 && )
+			compute(i, j);
 		}
 	}
-	
-
-
-
-
-
-
 
   	// done with MPI
   	MPI_Finalize();
 	return 0;
 }
-
